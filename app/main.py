b2a2a1f01ae8b3ea473c03e919f6cf6a0bae83c4
Jimmy Chen <jzc8@cornell.edu>
2019-04-11 02:20:37 -0400

    add some UI changes upon hover

--- app/main.py
+++ app/main.py
@@ -9,10 +9,9 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/home', methods=['GET', 'POST'])
 def show_home():
-	print("home root")
 	SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
-	csv_url = os.path.join(SITE_ROOT, "static/", "recommendations_with_names.csv")
-	song_id = "15O20RQyWJgKrkHID9ynT9"
+	csv_url = os.path.join(SITE_ROOT, "static/", "songs_with_recommendations_and_2d_proj_60k.csv")
+	song_id = "5pvJ59i7JxylN8VB24xdMs"
 	df = get_relevant_points(song_id, pd.read_csv(csv_url))
 	clustering_data = df.to_dict(orient='records')
 	clustering_data = json.dumps(clustering_data, indent=2)
@@ -20,35 +19,48 @@ def show_home():
 	return render_template('search_page.html', clustering_data = clustering_data)
 
 def get_relevant_points(song_id, df):
-	df1 = df[['id','cluster','song.0','song.1','song.2','song.3','song.4','song.5','song.6','song.7','song.8','song.9','X','Y']]
-	row = df1.loc[df1['id'] == song_id]
-
-	relevant_points = [row.at[0,'id'], row.at[0,'song.0'], row.at[0,'song.1'], row.at[0,'song.2'], row.at[0,'song.3'], row.at[0,'song.4'], row.at[0,'song.5'], \
-		row.at[0,'song.6'], row.at[0,'song.7'], row.at[0,'song.8'], row.at[0,'song.9']]
-
-	print("SKDF1JSLKDF1JL")
+	df1 = df[['song_id','cluster','song_1','song_2','song_3','song_4','song_5','song_6','song_7','song_8','song_9','song_10','x','y']]
+	row = df1.loc[df1['song_id'] == song_id]
 	print(row)
-	mask = df1['id'].isin(relevant_points)
+	print(row['song_id'].values[0])
+
+	relevant_points = [row['song_id'].values[0], row['song_1'].values[0], row['song_2'].values[0], row['song_3'].values[0], row['song_4'].values[0], row['song_5'].values[0], row['song_6'].values[0], \
+		row['song_7'].values[0], row['song_8'].values[0], row['song_9'].values[0], row['song_10'].values[0]]
+
+	print(row)
+	mask = df1['song_id'].isin(relevant_points)
 
 	df1 = df1.loc[mask]
-	min_x = df1['X'].min()
-	max_x = df1['X'].max()
-	min_y = df1['Y'].min()
-	max_y = df1['Y'].max()
-	relevant_rows = df[(df['X'] >= min_x - (0.01)) & (df['X'] <= max_x + 0.01) & (df['Y'] >= min_y - 0.01) & (df['Y'] <= max_y) + 0.01]
+	min_x = df1['x'].min()
+	max_x = df1['x'].max()
+	min_y = df1['y'].min()
+	max_y = df1['y'].max()
+	center_x = (min_x + max_x) / 2
+	center_y = (min_y + max_y) / 2
+	#relevant_rows = df[(df['x'] >= min_x - (0.4)) & (df['x'] <= max_x + 0.4) & (df['y'] >= min_y - 0.4) & (df['y'] <= max_y) + 0.4]
+	relevant_rows = df[(df['x'] - center_x)**2 + (df['y'] - center_y)**2 <= 0.10]
+	#relevant_rows = df
 	relevant_rows["top_10"] = False
-	relevant_rows['top_10'] = np.where(relevant_rows['id'].isin(relevant_points), True, False)
+	relevant_rows["top_10"] = np.where(relevant_rows['song_id'].isin(relevant_points), True, False)
+	#return df
 	return relevant_rows
 
 
 @app.route('/songSearchHandler', methods=['GET', 'POST'])
 def songSearchHandler():
-	print("NONONO")
-	song = request.json
-	print("SONG OSNG ")
-	print(song)
+	song_id = request.json
+	song_id = list(song_id.values())[0]
+	print(song_id)
+	print(type(song_id))
+	SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+	csv_url = os.path.join(SITE_ROOT, "static/", "songs_with_recommendations_and_2d_proj_60k.csv")
+	df = get_relevant_points(song_id, pd.read_csv(csv_url))
+	clustering_data = df.to_dict(orient='records')
+	clustering_data = json.dumps(clustering_data, indent=2)
+	data = {'clustering_data': clustering_data}
+	print("SONG SONG SONG")
 
-	return json.dumps({'status':'OK','id': song['song_ids'],'pass':"123"})
+	return json.dumps({'status':'OK','id': song_id,'relevant_points': clustering_data})
 
 @app.route("/james")
 def james():
